63ee7842522e121e875921379c5913a0b09f2126
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2018-09-08 18:34:04 +0200

    Update random.js

--- random.js
+++ random.js
@@ -6,7 +6,7 @@ function randomVehicle(query) {
     if (query === 'usage') {
         return 'Usage: !random (<class> | car | bike | land | air | sea | all [raceable])';
     }
-
+    return 'bla'+vehicles()[10];
     /* Find all vehicles that match the specified criteria */
     var vehicles = vehicles()/*.filter(function (vehicle) {
         return false;/*vehicle.matches(query);*/
