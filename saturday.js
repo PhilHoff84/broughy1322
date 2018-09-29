2d8f23b386085b6a20973a4c7a099a24adb36a1a
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2018-09-29 20:28:49 +0200

    Update saturday.js

--- saturday.js
+++ saturday.js
@@ -1,48 +1,48 @@
-function schedule(date, missedStreams) {
-	date.setUTCHours(date.getUTCHours() + 5, 0, 0, 0);
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=5 !saturday $(eval schedule(new Date(), 0, 0); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/saturday.js);)
+ */
+function schedule(date, missedStreams, missedPlatforms) {
+    date.setUTCHours(date.getUTCHours() + 5, 0, 0, 0);
 
-	var stream1 = nextStream(date);
-	var when1 = stream1.getUTCDay() !== 0 ? 'Saturday' : 'Sunday';
-	/* when1 += " "+ nth(stream1.getUTCDate()); */
-	var where1 = nextPlatform(stream1, missedStreams);
-  
-  	var stream2 = nextStream(nextDay(stream1));
-	var when2 = stream2.getUTCDay() !== 0 ? 'Saturday' : 'Sunday';
-	/* when2 += " "+ nth(stream2.getUTCDate()); */
-	var where2 = nextPlatform(stream2, missedStreams);
-	return 'Upcoming GTA Streams & Platforms: ' + when1 + ' on ' + where1 + ', ' + when2 + ' on ' + where2; 
+    var stream1 = nextStream(date);
+    var where1 = nextPlatform(stream1, missedStreams, missedPlatforms);
+
+    var stream2 = nextStream(nextDay(stream1));
+    var where2 = nextPlatform(stream2, missedStreams, missedPlatforms);
+    return 'Next Saturday evening stream: ' + where1 + '  | Following Saturday: ' + where2;
 }
 
-function nextPlatform(date, offset) {
-	var count = 0; /* count streams since origin */
-	var from = new Date(Date.UTC(2018, 7, 11));
-	var to = new Date(date); to.setUTCHours(0, 0, 0, 0);
-	while(from <= to) {
-		if(isStream(from)) {
-			count++;
-		}
-		nextDay(from);
-	}
-	var platform = ['XB1', 'PC', 'PS4'];
-	return platform[(count + offset) % platform.length];
+function nextPlatform(date, streamOffset, platformOffset) {
+    var count = 0; /* count streams since origin */
+    var from = new Date(Date.UTC(2018, 7, 11));
+    var to = new Date(date);
+    to.setUTCHours(0, 0, 0, 0);
+    while (from <= to) {
+        if (isStream(from)) {
+            count++;
+        }
+        nextDay(from);
+    }
+    if ((count + streamOffset) % 2 === 0) {
+        return 'Podcast on Twitch';
+    }
+    var platform = ['Content Creator XB1 on YouTube', 'Content Creator PC on YouTube', 'Content Creator PS4 on YouTube'];
+    return platform[(Math.floor(count / 2) + streamOffset + platformOffset) % platform.length];
 }
 
 function nextStream(date) {
-	var i = new Date(date);
-	while (!isStream(i)) {
-		nextDay(i);
-	}
-	return i; /* date of the next stream */
+    var i = new Date(date);
+    while (!isStream(i)) {
+        nextDay(i);
+    }
+    return i; /* date of the next stream */
 }
 
 function nextDay(date) {
-	return date.setDate(date.getDate() + 1);
+    return date.setUTCDate(date.getUTCDate() + 1);
 }
 
 function isStream(date) {
-	return date.getDay() % 6 === 0; /* Saturday or Sunday */
-}
-
-function nth(n) {
-	return n+(['st','nd','rd'][((n+90)%100-10)%10-1]||'th');
+    return date.getUTCDay() === 6; /* Saturday */
 }
