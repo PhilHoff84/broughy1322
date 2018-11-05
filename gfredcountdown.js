e1c9a5d3e5815890ff20af7b3975b9566cb94aba
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2018-11-05 21:01:29 +0100

    Add files via upload

---
+++ gfredcountdown.js
@@ -0,0 +1,66 @@
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=5 !gfredcountdown $(eval countdown(); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/gfredcountdown.js);)
+ */
+function countdown() {
+
+    var now = new Date();
+    now = new Date(Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate(),
+        now.getUTCHours(),
+        now.getUTCMinutes()
+    ));
+
+    /* Gfred starts at 22:30 */
+    var start = new Date(Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate() + (7 - now.getUTCDay()) % 7,
+        22,
+        30));
+
+    /* Gfred ends at 23:00 */
+    var end = new Date(Date.UTC(
+        now.getUTCFullYear(),
+        now.getUTCMonth(),
+        now.getUTCDate() + (7 - now.getUTCDay()) % 7,
+        23));
+
+    /* Gfred just ended; fast-forward to next week */
+    if (end < now) {
+        start.setUTCDate(start.getUTCDate() + 7);
+        end.setUTCDate(start.getUTCDate() + 7);
+    }
+
+    if (start <= now) {
+        return "The next live Gfred race will start in approximately… wait… it's happening right now!"
+    } else {
+        return 'The next live Gfred race will start in approximately ' + diff(start.getTime() - now.getTime());
+    }
+}
+
+function diff(difference_ms) {
+    /* 1 day in milliseconds */
+    var one_day = 1000 * 60 * 60 * 24;
+
+    /* remove milliseconds */
+    difference_ms = difference_ms / 1000;
+    var s = Math.floor(difference_ms % 60);
+    difference_ms = difference_ms / 60;
+    var m = Math.floor(difference_ms % 60);
+    difference_ms = difference_ms / 60;
+    var h = Math.floor(difference_ms % 24);
+    var d = Math.floor(difference_ms / 24);
+
+    var delta = [];
+    if (d > 0) delta.push(d + ' day' + (d > 1 ? 's' : ''));
+    if (h > 0) delta.push(h + ' hour' + (h > 1 ? 's' : ''));
+    if (m > 0) delta.push(m + ' minute' + (m > 1 ? 's' : ''));
+
+    if (delta.length === 3)
+        return delta.shift() + ', ' + delta.join(' and ');
+
+    return delta.join(', ');
+}
