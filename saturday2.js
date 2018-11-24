d80f0fcedcc9eb0c32e3121f8456a98a1ff38cbb
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2018-11-24 18:12:31 +0100

    Update saturday2.js

--- saturday2.js
+++ saturday2.js
@@ -3,23 +3,15 @@
  * !editcom -ul=everyone -cd=5 !saturday $(eval schedule(0, 0); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/saturday2.js);)
  */
 function schedule(missedStreams, missedPlatforms) {
-    var date = new Date();
-    date = new Date(Date.UTC(
-        date.getUTCFullYear(),
-        date.getUTCMonth(),
-        date.getUTCDate(),
-        date.getUTCHours() + 1,
-        date.getUTCMinutes(),
-        date.getUTCSeconds()
-    ));
+    var now = utcDate(new Date());
 
-    var stream1 = nextStream(date);
-    var what1 = nextPlatform(stream1, missedStreams, missedPlatforms);
+    var stream1 = nextStream(now);
+    var what1 = stream1.toUTCString()/*nth(stream1.getUTCDate())*/ +" "+ nextPlatform(stream1, missedStreams, missedPlatforms);
 
     var stream2 = nextStream(nextDay(stream1));
-    var what2 = nextPlatform(stream2, missedStreams, missedPlatforms);
+    var what2 = stream2.toUTCString()/*nth(stream1.getUTCDate())*/ +" "+ nextPlatform(stream2, missedStreams, missedPlatforms);
 
-    if (isVacationBetween(date, stream1)) {
+    if (isVacationBetween(now, stream1)) {
         return 'Next Saturday evening stream: Vacation | Following Saturday: ' + what2 + ' | All start at 7pm UK time';
     } else if (isVacationBetween(stream1, stream2)) {
         return 'Next Saturday evening stream: ' + what1 + ' | Following Saturday: Vacation | All start at 7pm UK time';
@@ -30,34 +22,42 @@ function schedule(missedStreams, missedPlatforms) {
 function nextPlatform(date, streamOffset, platformOffset) {
     var count = 0; /* count streams since origin */
     var from = new Date(Date.UTC(2018, 7, 11));
-    var to = new Date(date);
+    var to = utcDate(date);
     to.setUTCHours(0, 0, 0, 0);
 
     while (from <= to) {
         if (isStream(from)) {
             count++;
         }
-        nextDay(from);
+        from = nextDay(from);
     }
 
     if ((count + streamOffset) % 4 === 0) {
         return 'Podcast on Twitch';
     }
     var platforms = ['XB1', 'PC', 'PS4'];
-    var platform = platforms[count - (Math.floor(count / 4) + streamOffset + platformOffset) % platforms.length];
+    var platform = platforms[(count - Math.floor(count / 4) + streamOffset + platformOffset) % platforms.length];
     return 'Content Creator ' + platform + ' on YouTube';
 }
 
 function nextStream(date) {
-    var i = new Date(date);
+    var i = utcDate(date);
     while (!isStream(i)) {
-        nextDay(i);
+        i = nextDay(i);
     }
     return i; /* date of the next stream */
 }
 
 function nextDay(date) {
-    return date.setUTCDate(date.getUTCDate() + 1);
+    return new Date(Date.UTC(
+        date.getUTCFullYear(),
+        date.getUTCMonth(),
+        date.getUTCDate() + 1,
+        date.getUTCHours(),
+        date.getUTCMinutes(),
+        date.getUTCSeconds(),
+        date.getUTCMilliseconds()
+    ));
 }
 
 function isStream(date) {
@@ -65,12 +65,28 @@ function isStream(date) {
 }
 
 function isVacationBetween(from, to) {
-    var i = new Date(from);
+    var i = utcDate(from);
     while (i <= to) {
         if (i.getUTCDay() === 6 /* Saturday */ && i.getUTCDate() > 4 * 7 /* only 5th Saturday in a month */) {
             return true;
         }
-        nextDay(i);
+        i = nextDay(i);
     }
     return false;
+}
+
+function nth(n) {
+    return n+(['st','nd','rd'][((n+90)%100-10)%10-1]||'th');
+}
+
+function utcDate(date) {
+    return new Date(Date.UTC(
+        date.getUTCFullYear(),
+        date.getUTCMonth(),
+        date.getUTCDate(),
+        date.getUTCHours(),
+        date.getUTCMinutes(),
+        date.getUTCSeconds(),
+        date.getUTCMilliseconds()
+    ));
 }
