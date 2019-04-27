ab98981aa0bf76471b367fc45cb2ab012b72d8c5
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2019-04-27 16:55:06 +0200

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,7 +1,13 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=5 !schedule $(eval schedule('$(provider)', "$(urlfetch json https://docs.google.com/spreadsheets/d/1QDWpycJJFA-UAiSPIv-icJ4UZhbEmuN8wxxag83SE1c/export?exportFormat=csv)"); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/schedule.js);)
+ * !editcom -ul=everyone -cd=5 !schedule $(eval schedule('$(provider)', "$(urlfetch json https://docs.google.com/spreadsheets/d/10ahpRz_Q4aFSOMtacIZVJD3Urd1MlFDwi_KcdDrC6mc/export?exportFormat=tsv)"); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/schedule.js);)
  */
 function schedule(provider='', data = '') {
-    return provider + " @ " + data;
+    if (provider === 'twitch') {
+        return data;
+    }
+
+    return 'Twitch streams start at 18:00 UK time every Wednesday & Sunday plus 09:00 UK time every Saturday. '
+         + 'Full details are here: http://broughy.com/schedule '
+         + '(use !platform for next GTA stream platform info)';
 }
