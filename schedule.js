181a030465aadb76134a69fcb9bfdbc15b210c37
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2019-04-30 18:48:51 +0200

    Update schedule.js

--- schedule.js
+++ schedule.js
@@ -1,14 +1,36 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=5 !schedule $(eval schedule('$(provider)', "$(urlfetch json https://docs.google.com/spreadsheets/d/10ahpRz_Q4aFSOMtacIZVJD3Urd1MlFDwi_KcdDrC6mc/export?exportFormat=tsv)"); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/schedule.js);)
+ * !editcom -ul=everyone -cd=5 !schedule $(eval schedule('$(provider)', '$(query)', "$(urlfetch json https://docs.google.com/spreadsheets/d/1PT7RWCyBe0hIK7clZQoR6l9og7JwI--4ldKZit5UQFU/export?exportFormat=tsv)"); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/schedule.js);)
  */
-function schedule(provider='', data = '') {
+function schedule(provider='', query = '', data = '') {
+    query = normalize(query);
+
+    var rows = data.replace(/\t/g, ' | ').split('   ');
     if (provider === 'twitch') {
-        var rows = data.split("   ");
-        return (" • " + rows[0] + "\n • " + rows[1]).replace(/\t/g, ' | ');
+        return rows[query === 'next' ? 1 : 0];
+    }
+    return " • " + rows.join("\n • ");
+}
+
+function normalize(text) {
+    if (!text) {
+        text = '';
     }
 
-    return 'Twitch streams start at 18:00 UK time every Wednesday & Sunday plus 09:00 UK time every Saturday. '
-         + 'Full details are here: http://broughy.com/schedule '
-         + '(use !platform for next GTA stream platform info)';
+    /* Convert to lowercase */
+    text = text.toLowerCase();
+
+    /* Remove plural */
+    text = text.replace(/s\b/g, '');
+
+    /* Remove accents */
+    text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+    /* Remove everything behind '@' */
+    text = text.replace(/\s*@.*/, '');
+
+    /* Remove all chars that are not letters */
+    text = text.replace(/[^a-z]+/g, '');
+
+    return text;
 }
