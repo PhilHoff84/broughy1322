2bf884a905380b7e601dd14315411c8b38a46260
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2019-04-30 18:52:09 +0200

    Update and rename schedule.js to platform5.js

--- platform5.js
+++ platform5.js
@@ -1,8 +1,8 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=5 !schedule $(eval schedule('$(provider)', '$(query)', "$(urlfetch json https://docs.google.com/spreadsheets/d/1PT7RWCyBe0hIK7clZQoR6l9og7JwI--4ldKZit5UQFU/export?exportFormat=tsv)"); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/schedule.js);)
+ * !editcom -ul=everyone -cd=5 !platform $(eval platform('$(provider)', '$(query)', "$(urlfetch json https://docs.google.com/spreadsheets/d/1PT7RWCyBe0hIK7clZQoR6l9og7JwI--4ldKZit5UQFU/export?exportFormat=tsv)"); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/platform5.js);)
  */
-function schedule(provider='', query = '', data = '') {
+function platform(provider='', query = '', data = '') {
     query = normalize(query);
 
     var rows = data.replace(/\t/g, ' | ').split('   ');
