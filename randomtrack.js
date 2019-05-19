9e4c8ce0f612da52af84c0ab8af68f1df720fcc7
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2019-05-19 16:51:03 +0200

    Update randomtrack.js

--- randomtrack.js
+++ randomtrack.js
@@ -15,7 +15,7 @@ function track(provider='', query = '', data = '') {
     }
     
     
-    return output.length + ": " + output.join(' | ').substring(0, 380);
+    return output.length + ": " + output.join(', ').substring(0, 380);
     return 'n: '+data.split('\n').length + ' r: ' + data.split('\r').length;
 
     if (/\bps4\b/.test(query)) {
