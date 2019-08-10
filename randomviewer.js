2a92fc88db6385e549a80c75cbe70f6631756758
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2019-08-10 11:32:38 +0200

    Update randomviewer.js

--- randomviewer.js
+++ randomviewer.js
@@ -1,11 +1,11 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=30 !randomviewer $(eval viewer(`$(urlfetch json http://tmi.twitch.tv/group/user/broughy1322/chatters)`); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/randomviewer.js);)
+ * !editcom -ul=everyone -cd=30 !randomviewer $(eval viewer('$(user)', '$(urlfetch json http://tmi.twitch.tv/group/user/broughy1322/chatters)'); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/randomviewer.js);)
  */
-function viewer(text = '') {
+function viewer(user = '', text = '') {
     try {
-        chatters = JSON.parse(text).chatters;
-        viewers = chatters.viewers.filter(viewer => `$(user)`.toLowerCase() != viewer.toLowerCase());
+        user = user.toLowerCase();
+        var viewers = JSON.parse(text).chatters.viewers.filter(viewer => user.toLowerCase() != viewer.toLowerCase());
 
         if (viewers.length) {
             return "Thanks for watching " + viewers[Math.floor(Math.random() * viewers.length) + " broughyLove";
