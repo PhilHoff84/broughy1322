8b3e64d2b4a60f2b66e33d8f8e06215529ead703
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2020-09-14 14:11:55 +0200

    Add files via upload

---
+++ random-track-new.js
@@ -0,0 +1,76 @@
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=30 !randomtrack $(eval track('$(provider)', '$(query)', "$(urlfetch json https://docs.google.com/spreadsheets/d/15ZGd_KKINKJhqIS56Fy23YhY9DTeglmCoFYkREVFDek/export?exportFormat=tsv&gid=219652588)"); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-track-new.js);)
+ */
+function track(query = '', data = '') {
+    query = normalize(query);
+
+    const [platform_filter, type_filter] = parse_query(query);
+    if (platform_filter === '' || !data[platform_filter]) {
+        return 'Usage: !randomtrack (PS4 | XB1 | PC | 5M) (<category>)';
+    }
+
+    /* Find all tracks that match the specified platform */
+    const tracks = data[platform_filter];
+
+    /* Find all tracks that match the specified criteria */
+    var matching_tracks = [];
+    for (var track_type in data) {
+        if (normalize(track_type).indexOf(type_filter) === -1) {
+            continue;
+        }
+        matching_tracks = matching_tracks.concat(tracks[track_type]);
+        // Merge the second array into the first one
+        // Equivalent to vegetables.push('celery', 'beetroot')
+        //Array.prototype.push.apply(vegetables, moreVegs)
+    };
+
+    /* Output a random track (or error message) */
+    if (matching_tracks.length > 0) {
+        var i = Math.floor(Math.random() * matching_tracks.length);
+        var track = matching_tracks[i];
+        return 'Random Track: ' + track;
+    }
+    return 'Could not find a matching random track ¯\\_(ツ)_/¯';
+}
+
+function normalize(text) {
+    if (!text) {
+        text = '';
+    }
+
+    /* Convert to lowercase */
+    text = text.toLowerCase();
+
+    /* Remove plural (unless part of 'PS 4')*/
+    text = text.replace(/(?<=\bp)s\b(?=4)/g, '');
+
+    /* Remove accents */
+    text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+    /* Remove everything behind '@' */
+    text = text.replace(/\s*@.*/, '');
+
+    /* Remove all chars that are not letters */
+    text = text.replace(/[^a-z0-9 ]+/g, '');
+
+    return text;
+}
+
+function parse_query(query_before='') {
+    var query_after = '';
+
+    query_after = query_before.replace(/\bps ?4?\b/g, '');
+    if (query_before !== query_after) return ['ps', query_after.trim()];
+
+    query_after = query_before.replace(/\bpc\b/g, '');
+    if (query_before !== query_after) return ['pc', query_after.trim()];
+
+    query_after = query_before.replace(/\bxb(?:ox)? ?1?\b/g, '');
+    if (query_before !== query_after) return ['xb', query_after.trim()];
+
+    query_after = query_before.replace(/\b(?:5|five) ?m\b/g, '');
+    if (query_before !== query_after) return ['5m', query_after.trim()];
+
+    return ['', query_before];
+}
