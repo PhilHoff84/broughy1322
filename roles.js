00ac9aa42507e2884e0f453786478d2d862a7389
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2021-06-09 17:23:31 +0200

    Add files via upload

---
+++ roles.js
@@ -0,0 +1,10 @@
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=30 !roles $(eval roles('$(provider)'); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/roles.js);)
+ */
+function roles(provider='') {
+  if (provider === 'discord') {
+    return 'Please read the instructions in the #welcome channel to claim your *Patreon*, *YouTube*, *Twitch* or *Regular* role.'
+  }
+  return '';
+}
