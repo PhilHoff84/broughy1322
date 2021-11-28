dd048eaa62bfdff4fd1a21fa7f10c167a73c3975
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2021-11-28 16:20:45 +0100

    move emote

--- randomviewer.js
+++ randomviewer.js
@@ -1,22 +1,18 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=30 !randomviewer $(eval viewer('$(user)', '$(urlfetch json http://tmi.twitch.tv/group/user/broughy1322/chatters)'); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/randomviewer.js);)
+ * !editcom -ul=everyone -cd=30 !randomviewer $(eval viewer('$(user)', '$(urlfetch json http://tmi.twitch.tv/group/user/broughy1322/chatters)'); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/randomviewer.js);) broughyLove
  */
 function viewer(user = '', text = '') {
     try {
         user = user.toLowerCase();
-        
-        if (user==='broughy1322') {
-            return "Paint your Dubsta 2 chrome, if you want this to work broughyLove";
-        }
-        
+
         var chatters = JSON.parse(text).chatters;
         var viewers = chatters.viewers.concat(chatters.moderators).filter(function (viewer) {
             return viewer.toLowerCase() != user && viewer != 'nightbot' && viewer != 'streamelements';
         });
 
         if (viewers.length) {
-            return "Thanks for watching @" + viewers[Math.floor(Math.random() * viewers.length)] + " broughyLove";
+            return "Thanks for watching @" + viewers[Math.floor(Math.random() * viewers.length)];
         }
         return "No viewer"
     } catch(e) {
