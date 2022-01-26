b5ec2ab305749d978a8ea5361357dfb4c1a5b5b6
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2022-01-26 22:06:23 +0100

    Update random-meme.js

--- random-meme.js
+++ random-meme.js
@@ -1,6 +1,6 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=30 !randommeme $(eval meme(`$(urlfetch json https://docs.google.com/spreadsheets/d/1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0/export?format=tsv&id=1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0)`); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-meme.js);)
+ * !editcom -ul=everyone -cd=30 !randommeme $(eval meme(`$(urlfetch json https://docs.google.com/spreadsheets/d/1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0/export?format=tsv&id=1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0&gid=601678403)`); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-meme.js);)
  */
 function meme(data = '') {
     const memes = data.split('<EOL>');
