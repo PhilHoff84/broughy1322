aeda9b0c6029221f7a6c55fc6a96db2b5e627615
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2022-01-26 22:17:52 +0100

    Update random-clip.js

--- random-clip.js
+++ random-clip.js
@@ -1,6 +1,6 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=30 !randomclip $(eval clip(`$(urlfetch json https://docs.google.com/spreadsheets/d/1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0/export?format=tsv&id=1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0&gid=565945120)); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-clip.js);)
+ * !editcom -ul=everyone -cd=30 !randomclip $(eval clip(`$(urlfetch json https://docs.google.com/spreadsheets/d/1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0/export?format=tsv&id=1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0&gid=565945120)`); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-clip.js);)
  */
 function clip(data = '') {
     const clips = data.split('<EOL>');
