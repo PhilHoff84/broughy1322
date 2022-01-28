d0c58a7f55e3b4e9cb1572f2425b8eae8f4020e6
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2022-01-28 16:18:56 +0100

    Update random-random-video.js

--- random-random-video.js
+++ random-random-video.js
@@ -1,6 +1,6 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=30 !randomrandom $(eval random_video(`$(query)`, `$(urlfetch json https://docs.google.com/spreadsheets/d/1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0/export?format=tsv&id=1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0&gid=229679351)`); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-random-video.js);)
+ * !editcom -ul=everyone -cd=30 !randomrandom $(eval random_video(`$(query)`, `$(urlfetch json https://docs.google.com/spreadsheets/d/1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0/export?format=tsv&id=1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0&gid=1686137097)`); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-random-video.js);)
  */
 function random_video(query = '', data = '') {
     /* Sanitize the filter criteria specified in the query */
