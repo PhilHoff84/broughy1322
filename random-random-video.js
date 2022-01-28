77e3cef544004ebbed3ea6a811a0714607ce1f67
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2022-01-28 17:54:11 +0100

    Update random-random-video.js

--- random-random-video.js
+++ random-random-video.js
@@ -1,29 +1,15 @@
 /*
  * Nightbot command:
- * !editcom -ul=everyone -cd=30 !randomrandom $(eval random_video(`$(query)`, `$(urlfetch json https://docs.google.com/spreadsheets/d/1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0/export?format=tsv&id=1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0&gid=1686137097)`); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-random-video.js);)
+ * !editcom -ul=everyone -cd=30 !randomrandom $(eval random_video(`$(urlfetch json https://docs.google.com/spreadsheets/d/1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0/export?format=tsv&id=1xB2vwnA4CeqQcR6bO1tIvTjInVeodwB4nnYjrArQDF0&gid=1686137097)`); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/random-random-video.js);)
  */
-function random_video(query = '', data = '') {
-    /* Sanitize the filter criteria specified in the query */
-    const found = query.match(/[^@]*?(\d+)/);
-    const year = found && found[1];
+function random_video(data = '') {
+    const random_videos = data.split('<EOL>');
 
-    /* Parse videos */
-    const random_videos = data.split('<EOL>').reduce(function(accumulator, row) {
-        const cols = row.trim().split('\t');
-        if (!year || year === cols[0]) {
-            accumulator.push(cols[1]);
-        }
-        return accumulator;
-    }, new Array());
-
-    if (0 === random_videos.length) {
-        if (!year) {
-            return 'Could not find a video ¯\\_(ツ)_/¯';
-        }
-        return 'Could not find a video from ' + year + ' ¯\\_(ツ)_/¯';
+    if (random_videos.length > 1) {
+        const i = Math.floor(Math.random() * random_videos.length);
+        const random_video = random_videos[i];
+        return 'Random² Video: ' + random_video;
     }
 
-    const i = Math.floor(Math.random() * random_videos.length);
-    const random_video = random_videos[i];
-    return 'Random Video: ' + random_video;
+    return 'Could not find a video ¯\\_(ツ)_/¯';
 }
