5ac826a64e0427895a50c82926c25e5b05e7fbf0
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2022-04-19 18:10:00 +0200

    Create plans.js

---
+++ plans.js
@@ -0,0 +1,33 @@
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=5 !plans $(eval plans('$(provider)', '$(query)', "$(urlfetch json https://docs.google.com/spreadsheets/d/1PT7RWCyBe0hIK7clZQoR6l9og7JwI--4ldKZit5UQFU/export?exportFormat=tsv)"); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/plans.js);)
+ */
+function plans(provider='', query = '', data = '') {
+    query = normalize(query);
+
+    var rows = data.replace(/\t/g, ' | ').split('   ');
+    return rows[query === 'next' ? 1 : 0];
+}
+
+function normalize(text) {
+    if (!text) {
+        text = '';
+    }
+
+    /* Convert to lowercase */
+    text = text.toLowerCase();
+
+    /* Remove plural */
+    text = text.replace(/s\b/g, '');
+
+    /* Remove accents */
+    text = text.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+
+    /* Remove everything behind '@' */
+    text = text.replace(/\s*@.*/, '');
+
+    /* Remove all chars that are not letters */
+    text = text.replace(/[^a-z]+/g, '');
+
+    return text;
+}
