b3af73e3db96ef208bf801ded9f460d2e1ed497c
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2023-01-27 20:50:54 +0100

    Add PS5 & XSX

--- random-track.js
+++ random-track.js
@@ -9,7 +9,7 @@ function track(query = '', data = {}) {
     const [platform, category] = parse_query(query);
     const tracks = data[platform];
     if (tracks !== Object(tracks)) {
-        return 'Usage: !randomtrack (PS4 | XB1 | PC | 5M) (<category>)';
+        return 'Usage: !randomtrack (PS5 | PS4 | XSX | XB1 | PC | 5M) (<category>)';
     }
 
     /* Find all tracks that match the specified category */
@@ -58,13 +58,19 @@ function normalize(text) {
 function parse_query(query_before='') {
     var query_after = '';
 
-    query_after = query_before.replace(/\bps ?4?\b/g, '');
+    query_after = query_before.replace(/\bps ?5?\b/g, '');
+    if (query_before !== query_after) return ['PS5', query_after.trim()];
+
+    query_after = query_before.replace(/\bps ?4\b/g, '');
     if (query_before !== query_after) return ['PS4', query_after.trim()];
 
     query_after = query_before.replace(/\bpc\b/g, '');
     if (query_before !== query_after) return ['PC', query_after.trim()];
 
-    query_after = query_before.replace(/\bxb(?:ox)? ?1?\b/g, '');
+    query_after = query_before.replace(/\b?:(xb(?:ox)? ?|xsx)\b/g, '');
+    if (query_before !== query_after) return ['XSX', query_after.trim()];
+    
+    query_after = query_before.replace(/\bxb(?:ox)? ?1\b/g, '');
     if (query_before !== query_after) return ['XB1', query_after.trim()];
 
     query_after = query_before.replace(/\b(?:5|five) ?m\b/g, '');
