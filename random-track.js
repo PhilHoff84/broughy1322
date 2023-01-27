9f99d565893393d9f24c7fe0aa8a4fa12aa7055f
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2023-01-27 20:52:07 +0100

    fix XSX regex

--- random-track.js
+++ random-track.js
@@ -67,9 +67,9 @@ function parse_query(query_before='') {
     query_after = query_before.replace(/\bpc\b/g, '');
     if (query_before !== query_after) return ['PC', query_after.trim()];
 
-    query_after = query_before.replace(/\b?:(xb(?:ox)? ?|xsx)\b/g, '');
+    query_after = query_before.replace(/\b(?:xb(?:ox)? ?|xsx)\b/g, '');
     if (query_before !== query_after) return ['XSX', query_after.trim()];
-    
+
     query_after = query_before.replace(/\bxb(?:ox)? ?1\b/g, '');
     if (query_before !== query_after) return ['XB1', query_after.trim()];
 
