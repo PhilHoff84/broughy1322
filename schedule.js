51f582c03bfa253988a3e99b54a1f01aa0511ad9
PhilHoff84 <PhilHoff84@users.noreply.github.com>
2023-09-17 08:54:55 +0200

    Add files via upload

---
+++ schedule.js
@@ -0,0 +1,42 @@
+/*
+ * Nightbot command:
+ * !editcom -ul=everyone -cd=10 !schedule $(eval schedule('$(provider)'); $(urlfetch json https://raw.githubusercontent.com/PhilHoff84/broughy1322/master/schedule.js);)
+ */
+function schedule(provider);
+    if provider != "discord" {
+        return "Twitch streams start at 17:00 UK time every Wednesday & Sunday plus 09:00 UK time every Saturday. Full details are here: https://broughy.com/schedule (use !plans for games & GTA stream platform info)"
+    }
+
+    var now = new Date();
+    now = new Date(Date.UTC(
+      now.getUTCFullYear(),
+      now.getUTCMonth(),
+      now.getUTCDate()
+    ));
+
+    var wed=new Date(Date.UTC(
+      now.getUTCFullYear(),
+      now.getUTCMonth(),
+      now.getUTCDate() + (7 - now.getUTCDay()-4) % 7,
+      16,
+      00
+    ));
+
+    var sat=new Date(Date.UTC(
+      now.getUTCFullYear(),
+      now.getUTCMonth(),
+      now.getUTCDate() + (7 - now.getUTCDay()-1) % 7,
+      08,
+      00
+    ));
+
+    var sun=new Date(Date.UTC(
+      now.getUTCFullYear(),
+      now.getUTCMonth(),
+      now.getUTCDate() + (7 - now.getUTCDay()) % 7,
+      16,
+      00
+    ));
+
+    return "Twitch streams usually start at <t:${Math.floor(wed/1000)}:t> every Wednesday, <t:${Math.floor(sat/1000)}:t> every Saturday and <t:${Math.floor(sun/1000)}:t> every Sunday. Full details are here: https://broughy.com/schedule (use !plans for games & GTA stream platform info)"
+}
